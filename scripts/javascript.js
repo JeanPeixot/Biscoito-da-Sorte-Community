1c00256a73d9d6c67caa6521d10f3586def3c1c9
JeanPeixot <jeanpeixoto70@gmail.com>
2023-11-18 22:39:08 -0300

    implements cookie message after it is opened

---
+++ scripts/javascript.js
@@ -0,0 +1,14 @@
+function OpenCookie() {
+
+    let cookieclose = document.getElementById("cookieopen").classList.remove("cookiehidden")
+    let cookieopen = document.getElementById("cookieclose").classList.add("cookiehidden")
+    let maintitle = document.getElementById("maintitle")
+    maintitle.innerText = "Aqui está a sua sorte de hoje:"
+    let subtitle = document.getElementById("subtitle").classList.add("cookiehidden")
+    let message = document.getElementById("message").classList.add("fortunecookiemessage")
+    
+
+    
+    
+
+}
